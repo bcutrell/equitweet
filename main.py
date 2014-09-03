e8ecd3a4df3e8bd1bfe031278b0ac90c5d58bb2c
Ben Cutrell <bcutrell@dhcp-a-092.int.smartleaf.com>
2014-09-03 15:43:08 -0400

    re-org file structure

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#import code code.interact(local=locals())
-
 from lib.twitter_man import tman
+from lib.postgres_db import MyDB
 import lib.sentiment as senty
 import numpy as np
 import pandas as pd
+
+import code 
+
 
 # Get all S and P 500 data
 sp_data = pd.read_csv('data/constituents.csv')
@@ -14,6 +16,7 @@ results = tman.search.tweets(q="$ABT")
 
 # iter through search results
 for value in results.values()[1]:
+	code.interact(local=locals())	
 	print value['text']
 
 # iter through s&p tickers
