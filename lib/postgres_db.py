195f4ccb9a2ed35221802816c6189a274938fe6d
Michael McLellan <jmikem825@gmail.com>
2014-09-05 09:37:21 -0400

    Connect using given parameters instead of hardcoded string. Allow autocommit. Add fetchall and tables methods.

--- lib/postgres_db.py
+++ lib/postgres_db.py
@@ -1,24 +1,45 @@
 import psycopg2
 
+
 class MyDB(object):
 
- 	_db_connection = None
- 	_db_cur = None
 
-	def __init__(self):
-		## add dbname and user to config
-		self._db_connection = psycopg2.connect("dbname='demodb' user='bcutrell'")
-		self._db_cur = self._db_connection.cursor()
+    def __init__(self, **kwargs):
+        self._db_connection = psycopg2.connect(**kwargs)
+        self._db_cur = self._db_connection.cursor()
 
-	def query(self, query, params):
-		return self._db_cur.execute(query, params)
+        if kwargs.get('autocommit'):
+            self.autocommit = True
 
-	def fetchone(self):
-		return self._db_cur.fetchone()
+    def query(self, *args):
+        return self._db_cur.execute(*args)
 
-	def commit(self):
-		self._db_connection.commit()
+    def fetchone(self):
+        return self._db_cur.fetchone()
 
-	def __del__(self):
-  		self._db_connection.close()
-  		self._db_cur.close()
+    def fetchall(self):
+        return self._db_cur.fetchall()
+
+    def commit(self):
+        self._db_connection.commit()
+
+    @property
+    def autocommit(self):
+        return self._db_connection.autocommit
+
+    @autocommit.setter
+    def autocommit(self, value):
+        self._db_connection.autocommit = value
+
+    @property
+    def tables(self):
+        self.query('''
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+        ''')
+        return [x[0] for x in self.fetchall()]
+
+    def __del__(self):
+        self._db_cur.close()
+        self._db_connection.close()
