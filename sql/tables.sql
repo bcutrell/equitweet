b1c76e28a53be4051eeae1e991995eb5ff7846da
Michael McLellan <jmikem825@gmail.com>
2014-09-14 10:36:23 -0400

    Remove tabs. Add 'IF NOT EXISTS' qualifier. Add unique key to tweets table.

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,21 +1,24 @@
-CREATE TABLE stocks (
-	ticker	 varchar(8) primary key, -- name this stock?
-	name  varchar(100),
-	sector  varchar(100)
-);
-		
-CREATE TABLE tweets (
-	ticker   varchar(8) references stocks(ticker),
-	username varchar(20), -- increase length
-	tweet_id   bigint, -- id from twitter
-	followers_count  int,
-	polarity    double precision,
-	subjectivity    double precision,
-	date    date
+CREATE TABLE IF NOT EXISTS stocks (
+  ticker VARCHAR(8) primary key, -- name this stock?
+  name VARCHAR(100),
+  sector VARCHAR(100)
 );
 
-CREATE TABLE prices (
-	ticker   varchar(8) references stocks(ticker),
-	price 	real,
-	-- what stock info do we want?
-)
+CREATE TABLE IF NOT EXISTS tweets (
+  ticker   VARCHAR(8) REFERENCES stocks(ticker),
+  username VARCHAR(20), -- increase length
+  tweet_id BIGINT, -- id from twitter
+  followers_count INT,
+  polarity DOUBLE PRECISION,
+  subjectivity DOUBLE PRECISION,
+  date DATE,
+  UNIQUE (tweet_id, ticker)
+);
+
+CREATE TABLE IF NOT EXISTS prices (
+  ticker VARCHAR(8) REFERENCES stocks(ticker),
+  start_price REAL,
+  end_price REAL,
+  stat_date DATE
+  -- what stock info do we want?
+);
