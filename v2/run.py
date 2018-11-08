77ed05c41f4401d216cef27ecf134a75f7c21ca1
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-11-08 10:27:10 -0500

    adds batch search

--- v2/run.py
+++ v2/run.py
@@ -20,7 +20,16 @@ def search_tweets(query_string, max_id=None, since_id=None):
 
 def search_tweets_for_ticker(ticker):
     results = search_tweets('$' + ticker)
-    print(results['statuses'])
+    print(results['statuses'][0])
+
+def batch_search_tweets_for_tickers(tickers):
+    # Break into groups of 30 to keep query string below 500 chars
+    grouped_tickers = [tickers[i:i+30] for i in range(0, len(tickers), 30)]
+    for ticker_group in grouped_tickers:
+        query_string = ' OR '.join('$' + ticker for ticker in ticker_group)
+        results = search_tweets(query_string)
+        print(results['statuses'][0])
 
 if __name__ == '__main__':
-    search_tweets_for_ticker('A')
+    # search_tweets_for_ticker('A')
+    batch_search_tweets_for_tickers(['A', 'B'])
