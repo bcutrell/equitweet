31d22cf6ef3c29630da22c40df30eb974036aee4
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2018-11-08 10:34:51 -0500

    adds formatting

--- v2/run.py
+++ v2/run.py
@@ -1,7 +1,25 @@
 from twitter import Twitter, OAuth
 import config
+import code # code.interact(local=locals())
 
 twitter_client = Twitter(auth=OAuth(**config.TWITTER_CONFIG))
+
+def insert_sql_for_tweets(tweets):
+    pass
+
+def format_tweets(tweets):
+    return [ format_tweet(tweet) for tweet in tweets ]
+
+def format_tweet(tweet):
+    return {
+        'text': tweet['text'],
+        'tweet_id': tweet['id'],
+        'username': tweet['user']['name'],
+        'followers': tweet['user']['followers_count'],
+        'created_date': tweet['created_at'],
+        'retweet_count': tweet['retweet_count'],
+        'favorite_count': tweet['favorite_count']
+    }
 
 def search_tweets(query_string, max_id=None, since_id=None):
     query_args = {
@@ -20,7 +38,7 @@ def search_tweets(query_string, max_id=None, since_id=None):
 
 def search_tweets_for_ticker(ticker):
     results = search_tweets('$' + ticker)
-    print(results['statuses'][0])
+    return format_tweets(results['statuses'])
 
 def batch_search_tweets_for_tickers(tickers):
     # Break into groups of 30 to keep query string below 500 chars
@@ -31,5 +49,6 @@ def batch_search_tweets_for_tickers(tickers):
         print(results['statuses'][0])
 
 if __name__ == '__main__':
-    # search_tweets_for_ticker('A')
-    batch_search_tweets_for_tickers(['A', 'B'])
+    tweets = search_tweets_for_ticker('A')
+    print(tweets[0])
+    # batch_search_tweets_for_tickers(['A', 'B'])
